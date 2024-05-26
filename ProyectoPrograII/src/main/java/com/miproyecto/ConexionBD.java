7198dc41227dad28d37a96a88bcc676e58315177
ThonyV.N <91163138+sumforthefool@users.noreply.github.com>
2024-05-25 18:26:51 -0600

    Create Conexion to DB
    Se crea una conexion basica y estandar, puede que no sea funcional, para la BD de Oracle 21X

--- ProyectoPrograII/src/main/java/com/miproyecto/ConexionBD.java
+++ ProyectoPrograII/src/main/java/com/miproyecto/ConexionBD.java
@@ -1,4 +1,16 @@
 package com.miproyecto;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBD {
+
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+    private static final String USUARIO = "SYSTEM";
+    private static final String CONTRASENA = "nthn";
+
+    public static Connection obtenerConexion() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+    }
 }
