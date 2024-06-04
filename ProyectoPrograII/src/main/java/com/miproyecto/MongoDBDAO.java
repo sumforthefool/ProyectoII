3d254043abdefd6345d2d26895e7a91ceee916c7
ThonyV.N <91163138+sumforthefool@users.noreply.github.com>
2024-06-03 22:02:54 -0600

    Add and upgrade MongoDBDAO
    Se agregan actualizaciones y mejoras a la clase MongoDBDAO

--- ProyectoPrograII/src/main/java/com/miproyecto/MongoDBDAO.java
+++ ProyectoPrograII/src/main/java/com/miproyecto/MongoDBDAO.java
@@ -4,8 +4,10 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
-import javax.swing.text.Document;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MongoDBDAO {
 
@@ -19,6 +21,56 @@ public class MongoDBDAO {
         database  = mongoClient.getDatabase("PruebaP2");
         collection = database.getCollection("Persona");
 
+    }
+
+    public void insertarPersona(Persona persona) {
+        Document document = new Document("_id", persona.getId())
+                .append("nombre", persona.getNombre())
+                .append("apellido", persona.getApellido())
+                .append("edad", persona.getEdad());
+
+        collection.insertOne(document);
+    }
+
+    public Persona obtenerPersonaPorId(int id) {
+        Document document = collection.find(new Document("_id", id)).first();
+        if (document != null) {
+            return new Persona(
+                    document.getInteger("_id"),
+                    document.getString("nombre"),
+                    document.getString("apellido"),
+                    document.getInteger("edad")
+            );
+        } else {
+            return null;
+        }
+    }
+
+    public List<Persona> obtenerTodasLasPersonas() {
+        List<Persona> personas = new ArrayList<>();
+        for (Document document : collection.find()) {
+            personas.add(new Persona(
+                    document.getInteger("_id"),
+                    document.getString("nombre"),
+                    document.getString("apellido"),
+                    document.getInteger("edad")
+            ));
+        }
+        return personas;
+    }
+
+    public void actualizarPersona(Persona persona) {
+        Document filter = new Document("_id", persona.getId());
+        Document update = new Document("$set",
+                new Document("nombre", persona.getNombre())
+                        .append("apellido", persona.getApellido())
+                        .append("edad", persona.getEdad()));
+        collection.updateOne(filter, update);
+    }
+
+    public void eliminarPersona(int id) {
+        Document filter = new Document("_id", id);
+        collection.deleteOne(filter);
     }
 
 }
