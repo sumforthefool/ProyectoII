005005110de121c0289e04effc98b2ea9f4e7b6b
ThonyV.N <91163138+sumforthefool@users.noreply.github.com>
2024-06-03 19:29:19 -0600

    Create .env
    Se crea el archivo .env para guardar la informacion que no debe ser mostrada al publico

--- ProyectoPrograII/src/main/java/com/miproyecto/ConexionBD.java
+++ ProyectoPrograII/src/main/java/com/miproyecto/ConexionBD.java
@@ -3,12 +3,20 @@ package com.miproyecto;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class ConexionBD {
 
-    private static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
-    private static final String USUARIO = "SYSTEM";
-    private static final String CONTRASENA = "nthn2001";
+    private static final String URL;
+    private static final String USUARIO;
+    private static final String CONTRASENA;
+
+    static {
+        Dotenv dotenv = Dotenv.load();
+        URL = dotenv.get("DATABASE_URL");
+        USUARIO = dotenv.get("DATABASE_USER");
+        CONTRASENA = dotenv.get("DATABASE_PASSWORD");
+    }
 
     public static Connection obtenerConexion() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, CONTRASENA);
